673bb0c14bc5af8349181e4a920324e35ae48a70
balenciaga <jamshidedits@gmail.com>
2025-02-23 20:46:33 +0500

    Create Home.vue

---
+++ src/views/Home.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+        import Hero from '@/components/blocks/Hero.vue';
+        import Projects from '@/components/blocks/Projects.vue';
+</script>
+
+<template>
+        <main>
+                <Hero/>
+                <Projects/>
+        </main>
+</template>
