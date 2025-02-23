2e056bf48ebecdfd49706a1d8aaba9e32efb9e6c
balenciaga <jamshidedits@gmail.com>
2025-02-23 20:45:38 +0500

    Create Projects.vue

---
+++ src/components/blocks/Projects.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+        import router from '@/router';
+        import Button from '../Button.vue';
+        
+        const projects = () => {
+                router.push("/projects")
+        }
+</script>
+
+<template>
+        <section class="projects container">
+                <div class="projects-title section-title">
+                        <h1>последние работы</h1>
+                </div>
+                <div class="mt-[45px]">
+                        <Button text="последние работы" color="white" :func="projects" icon="/images/icons/arrow-right.svg"/>
+                </div>
+        </section>
+</template>
